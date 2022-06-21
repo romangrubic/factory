170906701cdf2cbe8582bfe5a8f4829ea0b87914
Roman Grubic <grubic.roman@gmail.com>
2022-06-21 14:29:27 +0200

    Added 'tags' filter logic and small refactoring.

--- src/Repository/MealsRepository.php
+++ src/Repository/MealsRepository.php
@@ -1,11 +1,14 @@
 <?php
 
+/**
+ * This file contains Meals repository methods
+ */
+
 namespace App\Repository;
 
 use App\Entity\Meals;
 use Carbon\Carbon;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -18,12 +21,10 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class MealsRepository extends ServiceEntityRepository
 {
-    private $em;
 
-    public function __construct(ManagerRegistry $registry, EntityManagerInterface $em)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Meals::class);
-        $this->em = $em;
     }
 
     public function add(Meals $entity, bool $flush = false): void
@@ -43,12 +44,17 @@ class MealsRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
+    
+    /**
+     * Main method for getting data filtered by parameters array
+     *
+     * @param  array $parameters
+     * @return void
+     */
     public function getMeals($parameters)
     {
-        // dd($parameters);
-        $q = $this->createQueryBuilder('m')
-            ->select('m');
+        $query = $this->createQueryBuilder('m')
+                ->select('m');
 
         /**
          * With filter
@@ -57,18 +63,18 @@ class MealsRepository extends ServiceEntityRepository
             $with = array_filter(explode(',', $parameters['with']));
 
             if (in_array('tags', $with)) {
-                $q->leftJoin('m.tags', 't')
-                    ->addSelect('t');
+                $query->leftJoin('m.tags', 't');
+                    // ->addSelect('t');
             }
 
             if (in_array('ingredients', $with)) {
-                $q->leftJoin('m.ingredients', 'i')
-                    ->addSelect('i');
+                $query->leftJoin('m.ingredients', 'i');
+                    // ->addSelect('i');
             }
 
             if (in_array('category', $with)) {
-                $q->leftJoin('m.category_id', 'c', 'with', 'm.category_id = c.id')
-                    ->addSelect('c');
+                $query->leftJoin('m.category_id', 'c', 'with', 'm.category_id = c.id');
+                    // ->addSelect('c');
             }
         }
 
@@ -78,56 +84,47 @@ class MealsRepository extends ServiceEntityRepository
         if (isset($parameters['category'])) {
             switch ($parameters['category']) {
                 case('NULL'):
-                    $q->andWhere('m.category_id IS NULL');
+                    $query->andWhere('m.category_id IS NULL');
                     break;
                 case('!NULL'):
-                    $q->andWhere('m.category_id IS NOT NULL');
+                    $query->andWhere('m.category_id IS NOT NULL');
                     break;
                 default:
-                    $q->andWhere('m.category_id = '. $parameters['category']);
+                    $query->andWhere('m.category_id = '. $parameters['category']);
                     break;
             }
         }
-
-        // if (isset($parameters['tags'])) {
-        //     $tagArray = explode(',', $parameters['tags']);
-        //     $count = count($tagArray);
-            
-        //     $q->leftJoin('m.tags', 't')
-        //             ->addSelect('t');
-
-        //     $q->andWhere($q->expr()->exists('SELECT 1
-        //                         FROM meals_tags mt
-        //                         WHERE m.id = mt.meals AND
-        //                             mt.tags IN (14)
-        //                             HAVING COUNT(1) = 1'));
-        // }
 
         /**
          * Diff_time filter
          */
         if (isset($parameters['diff_time'])) {
-            /**
-             * Getting all data, inlcuding soft deleted.
-             * Therefore, disabling the filter
-             */
-            $this->em->getFilters()->disable('softdeleteable');
-
             $timestamp = Carbon::createFromTimestamp($parameters['diff_time']);
 
-            $q->andWhere('m.updated_at >= :timestamp')
+            $query->andWhere('m.updated_at >= :timestamp')
               ->setParameter('timestamp', $timestamp);
-
-            /**
-             * Enabling the filter after it.
-             */
-            $this->em->getFilters()->enable('softdeleteable');
         }
 
-        $query = $q->getQuery();
-        
-        // dd($query);
+        /**
+         * Tags filter
+         */
+        if (isset($parameters['tags'])) {
+            $tagArray = explode(',', $parameters['tags']);
+            
+            $query->andWhere($query->expr()->exists('SELECT 1
+                                            FROM App\Entity\MealsTags mt
+                                            WHERE m.id = mt.meals 
+                                            AND mt.tags IN (:tagsArray)
+                                            HAVING COUNT(1) = :count'))
+                  ->setParameters([
+                        'tagsArray' => $tagArray,
+                        'count' => count($tagArray)
+                    ]);
+        }
 
-        return $query;
+        /**
+         * Return query for paginator (not results!)
+         */
+        return $query->getQuery();
     }
 }
